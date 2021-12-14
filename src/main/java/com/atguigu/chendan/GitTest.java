224bbd38b4203d607bce0caa1717c899e57ec6ca
chendan <chendan0703@qq.com>
2021-12-15 01:17:02 +0800

    third commit

--- src/main/java/com/atguigu/chendan/GitTest.java
+++ src/main/java/com/atguigu/chendan/GitTest.java
@@ -8,5 +8,7 @@ package com.atguigu.chendan;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 2!!!");
+        System.out.println("hello git 3!!!");
+        System.out.println("hello git 4!!!");
     }
 }
