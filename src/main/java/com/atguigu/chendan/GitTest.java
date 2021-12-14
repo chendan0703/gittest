41fd827e6db2dd7eeca1c865492827acbbffc40d
chendan <chendan0703@qq.com>
2021-12-15 01:13:54 +0800

    second commit

--- src/main/java/com/atguigu/chendan/GitTest.java
+++ src/main/java/com/atguigu/chendan/GitTest.java
@@ -7,6 +7,6 @@ package com.atguigu.chendan;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!!!");
+        System.out.println("hello git 2!!!");
     }
 }
